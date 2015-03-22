55d4b5a422181f5b384903ccd89bf91c44f6614e
Nicholas Mitchell <mitchell.nicholas@yahoo.com>
2015-03-22 12:44:06 -0700

    first add

---
+++ javascript.js
@@ -0,0 +1,32 @@
+
+window.onload = function(){
+	document.getElementById("commboxtype").innerHTML= "Pick Rock Paper or Scissors to play the computer to five";
+
+	document.getElementById("playerscore").innerHTML= "Player<br>Score<br>0";
+
+	document.getElementById("computerscore").innerHTML= "Computer<br>Score<br>0";
+
+	document.getElementById("completebox").innerHTML= "Winner / Loser";
+};
+
+
+
+
+/*function myDeclaredFunc(greeting){
+	return greeting + "I was created by a function";
+}
+
+var myExpressedFunc = function(greeting){
+	return greeting + "I was created by a function expression";
+}
+
+console.log(myExpressedFunc("Hello, "));
+
+var letMeUseAnotherFunction = function(myFunc){
+	console.log(myFunc("dude,yeah!"));
+};
+
+letMeUseAnotherFunction(5);
+letMeUseAnotherFunction("dude");
+letMeUseAnotherFunction([1,3,5,7]);
+letMeUseAnotherFunction(myDeclaredFunc);*/
