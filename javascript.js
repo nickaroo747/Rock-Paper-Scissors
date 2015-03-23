13ff2c2735cf720670756863e8b0d859c0822041
Nicholas Mitchell <mitchell.nicholas@yahoo.com>
2015-03-22 18:42:22 -0700

    new updates

--- javascript.js
+++ javascript.js
@@ -1,32 +1,68 @@
-
+/////////////////////////////////////////////////
+/* Window on loads + Begining text instructions*/
+/////////////////////////////////////////////////
 window.onload = function(){
-	document.getElementById("commboxtype").innerHTML= "Pick Rock Paper or Scissors to play the computer to five";
+	document.getElementById("box1A").innerHTML= "Player Picks:<br>";
 
-	document.getElementById("playerscore").innerHTML= "Player<br>Score<br>0";
+	document.getElementById("box1B").innerHTML= "Computer Picks:<br>";
 
-	document.getElementById("computerscore").innerHTML= "Computer<br>Score<br>0";
+	document.getElementById("scorebox1").innerHTML= "Player Score:";
+
+	document.getElementById("scorebox2").innerHTML= "Computer Score:";
+
+	document.getElementById("playerscore").innerHTML= "0";
+
+	document.getElementById("computerscore").innerHTML= "0";
+
 
 	document.getElementById("completebox").innerHTML= "Winner / Loser";
 };
-
-
-
-
-/*function myDeclaredFunc(greeting){
-	return greeting + "I was created by a function";
-}
-
-var myExpressedFunc = function(greeting){
-	return greeting + "I was created by a function expression";
-}
-
-console.log(myExpressedFunc("Hello, "));
-
-var letMeUseAnotherFunction = function(myFunc){
-	console.log(myFunc("dude,yeah!"));
+///////////////////////////////
+/* Html Button value comes in*/
+///////////////////////////////
+function pickRock (){
+	document.getElementById("box2A").innerHTML= "Rock";
+	// detWin();
+	// playToFive();
 };
 
-letMeUseAnotherFunction(5);
-letMeUseAnotherFunction("dude");
-letMeUseAnotherFunction([1,3,5,7]);
-letMeUseAnotherFunction(myDeclaredFunc);*/
+function pickPaper (){
+	document.getElementById("box2A").innerHTML= "Paper";
+	// detWin();
+	// playToFive();
+};
+
+function pickScissors (){
+	document.getElementById("box2A").innerHTML= "Scissors";
+	// detWin();
+	// playToFive();
+};
+// /////////////////////////////////////////
+// /* Computer's randomized value comes in*/
+// /////////////////////////////////////////
+var nameArray = ["Rock", "Paper", "Scissors"];
+
+function randomPick (){
+	var x = Math.floor(Math.random() * nameArray.length);
+	rname = (nameArray[x]);
+	document.getElementById("box2B").innerHTML= rname
+};
+// ///////////////////////////////////////////////////
+// /* The two values are compared against each other*/
+// ///////////////////////////////////////////////////
+// var x = function detWin (){
+// 	if (document.getElementById("box2A") == "Paper" && document.getElementById("box2B") = "Rock" {document.getElementById("playerscore").innerHTML= "Player<br>Score<br> i++"};
+// 	if (document.getElementById("box2A") == "Rock" && document.getElementById("box2B") = "Scissors" {document.getElementById("playerscore").innerHTML= "Player<br>Score<br> i++"};
+// 	if (document.getElementById("box2A") == "Scissors" && document.getElementById("box2B") = "paper" {document.getElementById("playerscore").innerHTML= "Player<br>Score<br> i++"};
+
+// 	if (document.getElementById("box2A") == "Paper" && document.getElementById("box2B") = "Rock" {document.getElementById("playerscore").innerHTML= "Computer<br>Score<br> i++"};
+// 	if (document.getElementById("box2A") == "Rock" && document.getElementById("box2B") = "Scissors" {document.getElementById("playerscore").innerHTML= "Computer<br>Score<br> i++"};
+// 	if (document.getElementById("box2A") == "Scissors" && document.getElementById("box2B") = "paper" {document.getElementById("playerscore").innerHTML= "Computer<br>Score<br> i++"};
+// };
+// /////////////////
+// /* Play to Five*/
+// /////////////////
+
+// function playToFive() {
+//      if (document.getElementById("playerscore").innerHTML == "Computer<br>Score<br> i++" = "paper" {document.getElementById("playerscore").innerHTML= "Computer<br>Score<br> i++"};
+//   }
