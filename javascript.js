e15af8ceea1e4d72beb4fceabecd4095a57bd4aa
Nicholas Mitchell <mitchell.nicholas@yahoo.com>
2015-03-30 12:09:56 -0700

    updates

--- javascript.js
+++ javascript.js
@@ -1,68 +1,114 @@
-/////////////////////////////////////////////////
-/* Window on loads + Begining text instructions*/
-/////////////////////////////////////////////////
-window.onload = function(){
-	document.getElementById("box1A").innerHTML= "Player Picks:<br>";
 
-	document.getElementById("box1B").innerHTML= "Computer Picks:<br>";
-
-	document.getElementById("scorebox1").innerHTML= "Player Score:";
-
-	document.getElementById("scorebox2").innerHTML= "Computer Score:";
-
-	document.getElementById("playerscore").innerHTML= "0";
-
-	document.getElementById("computerscore").innerHTML= "0";
-
-
+	var playerScore = document.getElementById("playerScore");
+	var computerScore = document.getElementById("computerScore");
 	document.getElementById("completebox").innerHTML= "Winner / Loser";
-};
-///////////////////////////////
-/* Html Button value comes in*/
-///////////////////////////////
-function pickRock (){
-	document.getElementById("box2A").innerHTML= "Rock";
-	// detWin();
-	// playToFive();
-};
 
-function pickPaper (){
-	document.getElementById("box2A").innerHTML= "Paper";
-	// detWin();
-	// playToFive();
-};
+	var box1 = document.getElementById("playerScore").innerText = "0";
+	var box2 = document.getElementById("computerScore").innerText = "0";
+	var playerPlays = document.getElementById("playerMove");
+	var box4 = document.getElementById("computerMove");
 
-function pickScissors (){
-	document.getElementById("box2A").innerHTML= "Scissors";
-	// detWin();
-	// playToFive();
-};
-// /////////////////////////////////////////
-// /* Computer's randomized value comes in*/
-// /////////////////////////////////////////
-var nameArray = ["Rock", "Paper", "Scissors"];
+	var rockBtn = document.getElementById("rock");
+	var paperBtn = document.getElementById("paper");
+	var scissorsBtn = document.getElementById("scissors");
+	
+	var playerWins = 0;
+	var computerWins = 0;
+
+	var computerMove;
+
+/* Computer's randomized value comes in*/
+
+var nameArray = ["rock", "paper", "scissors"];
 
 function randomPick (){
 	var x = Math.floor(Math.random() * nameArray.length);
 	rname = (nameArray[x]);
-	document.getElementById("box2B").innerHTML= rname
+	document.getElementById("computerMove").innerHTML= rname;
+	return rname;
 };
-// ///////////////////////////////////////////////////
-// /* The two values are compared against each other*/
-// ///////////////////////////////////////////////////
-// var x = function detWin (){
-// 	if (document.getElementById("box2A") == "Paper" && document.getElementById("box2B") = "Rock" {document.getElementById("playerscore").innerHTML= "Player<br>Score<br> i++"};
-// 	if (document.getElementById("box2A") == "Rock" && document.getElementById("box2B") = "Scissors" {document.getElementById("playerscore").innerHTML= "Player<br>Score<br> i++"};
-// 	if (document.getElementById("box2A") == "Scissors" && document.getElementById("box2B") = "paper" {document.getElementById("playerscore").innerHTML= "Player<br>Score<br> i++"};
+/* The two values are compared against each other*/
 
-// 	if (document.getElementById("box2A") == "Paper" && document.getElementById("box2B") = "Rock" {document.getElementById("playerscore").innerHTML= "Computer<br>Score<br> i++"};
-// 	if (document.getElementById("box2A") == "Rock" && document.getElementById("box2B") = "Scissors" {document.getElementById("playerscore").innerHTML= "Computer<br>Score<br> i++"};
-// 	if (document.getElementById("box2A") == "Scissors" && document.getElementById("box2B") = "paper" {document.getElementById("playerscore").innerHTML= "Computer<br>Score<br> i++"};
-// };
-// /////////////////
-// /* Play to Five*/
-// /////////////////
+function getWinner(playerMove, computerMove) {
+	if(
+		playerMove === 'rock' && computerMove === 'paper' ||
+		playerMove === 'scissors' && computerMove === 'rock' ||
+		playerMove === 'paper' && computerMove === 'scissors') {
+		computerWins++;
+		whoWins(computerWins);
+		// console.log('computer wins');
+		computerScore.innerHTML = computerWins;
+		// console.log(computerWins++);
+		return 'computer wins';
+	
+	} else if (playerMove === computerMove){
+	
+		console.log('tie');
+		return 'tie';
 
-// function playToFive() {
-//      if (document.getElementById("playerscore").innerHTML == "Computer<br>Score<br> i++" = "paper" {document.getElementById("playerscore").innerHTML= "Computer<br>Score<br> i++"};
-//   }
+	} else {
+		playerWins++;
+		whoWins(playerWins);
+		// console.log('player wins');
+		playerScore.innerHTML = playerWins;
+		// console.log(playerWins++);
+		return 'player wins';
+	}
+	console.log(playerMove);
+}
+
+	function theGame(move){
+		var winner = getWinner(move, randomPick());
+		playerPlays.innerText = move;
+		// if winner = getWinner {dothis}
+	};
+
+	rockBtn.addEventListener('click', function(){
+		theGame('rock');
+
+	});
+
+	paperBtn.addEventListener('click', function(){
+		theGame('paper');
+
+	});
+
+	scissorsBtn.addEventListener('click', function(){
+		theGame('scissors');
+
+	});
+
+
+function whoWins(score) {
+	if( score === 5) {
+		console.log("Player Wins: " + playerWins);
+		if (playerWins > computerWins){
+			document.getElementById("completebox").innerHTML= "Winner";
+			document.getElementById("rock").disabled = true;
+			document.getElementById("paper").disabled = true;
+			document.getElementById("scissors").disabled = true;
+		}
+		else {document.getElementById("completebox").innerHTML= "Loser";
+			document.getElementById("rock").disabled = true;
+			document.getElementById("paper").disabled = true;
+			document.getElementById("scissors").disabled = true;
+		}	
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
